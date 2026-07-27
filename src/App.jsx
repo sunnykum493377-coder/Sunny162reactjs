62d098f591f4f144e3432906b579e5deb617607d
sunny kumar <sunnykum493377@gmail.com>
2026-07-27 14:32:32 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'  
+import Navbar from
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
