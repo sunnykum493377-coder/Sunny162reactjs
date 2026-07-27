f7bef9ec10c459fd94a0d48b6c583395a6e93278
sunny kumar <sunnykum493377@gmail.com>
2026-07-27 14:50:35 +0530

    new commit

---
+++ src/features/Home/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='homepage'style={{backgroundColor:'black',height:'100vh'}}>
+        
+    </div>
+  )
+}
+
+export default Homepage
